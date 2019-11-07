ddb55d02dbeb6ce1d0b88afc2e694ee52f0f1d29
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-11-06 16:10:13 -0800

    initial commit

---
+++ hello-world/hello_world.go
@@ -0,0 +1,18 @@
+// package main //1
+//
+// import "fmt" //2
+//
+// func main() { //3
+//     fmt.Println("Hello, World!") //4
+// }
+
+// why not package main?
+// ----- STUB SOLUTION -----
+package greeting
+
+// Go "HelloWorld"
+func HelloWorld() string {
+
+	return "Hello, World!"
+}
+// ----- STUB SOLUTION -----
